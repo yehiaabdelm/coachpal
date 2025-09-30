8b2b3388ac1c4b69cd11d03ac4f15fefdffe08a9
yehiaabdelm <yehiaabdelm@gmail.com>
2025-09-30 16:33:15 +0300

    remove unnecessary import

--- web/src/lib/components/nav-user.svelte
+++ web/src/lib/components/nav-user.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
-	import BadgeCheckIcon from '@lucide/svelte/icons/badge-check';
-	import BellIcon from '@lucide/svelte/icons/bell';
 	import ChevronsUpDownIcon from '@lucide/svelte/icons/chevrons-up-down';
-	import CreditCardIcon from '@lucide/svelte/icons/credit-card';
 	import LogOutIcon from '@lucide/svelte/icons/log-out';
-	import SparklesIcon from '@lucide/svelte/icons/sparkles';
 
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
-	import { sliderClasses } from '@mui/material';
 
 	let {
 		user
