026c40c494570a969452b3af7893f6c30dd60d96
yehiaabdelm <yehiaabdelm@gmail.com>
2025-10-02 21:34:32 +0300

    no-verify

--- api/src/db/index.ts
+++ api/src/db/index.ts
@@ -1,12 +1,10 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema.js"
+import * as schema from "./schema.js";
 import pg from "pg";
-
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
 });
 
 export default drizzle(pool, { schema, casing: "snake_case" });
