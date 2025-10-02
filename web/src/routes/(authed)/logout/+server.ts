6f6d211e08747cec0720c1dac1349ee5bf3aa01b
yehiaabdelm <yehiaabdelm@gmail.com>
2025-10-02 22:38:23 +0300

    fix logout

--- web/src/routes/(authed)/logout/+server.ts
+++ web/src/routes/(authed)/logout/+server.ts
@@ -1,8 +1,12 @@
 import type { RequestHandler } from './$types';
 import { redirect } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
 export const GET: RequestHandler = async ({ cookies }) => {
 	cookies.delete('token', {
-		path: '/'
+		path: '/',
+		sameSite: env.NODE_ENV === 'production' ? 'none' : 'lax',
+		secure: env.NODE_ENV === 'production',
+		domain: env.NODE_ENV === 'production' ? '.dev.coachpal.app' : undefined
 	});
 	redirect(303, '/login');
 };
