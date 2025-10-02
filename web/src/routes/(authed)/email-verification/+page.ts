82730a36bbf608b7bc3f6b69087250aa413e97ce
yehiaabdelm <yehiaabdelm@gmail.com>
2025-10-02 21:48:44 +0300

    remove layout

--- web/src/routes/(authed)/email-verification/+page.ts
+++ web/src/routes/(authed)/email-verification/+page.ts
@@ -1,17 +1,17 @@
-import type { LayoutLoad } from './$types';
+import type { PageLoad } from './$types';
 import { PUBLIC_API_URL } from '$env/static/public';
-import type { User } from '../../types';
 import { redirect } from '@sveltejs/kit';
+import type { User } from '../../../types';
+
 export const load = (async ({ fetch }) => {
 	const response = await fetch(`${PUBLIC_API_URL}/users/me`, {
 		credentials: 'include'
 	});
-
-	if (!response.ok) {
-		redirect(303, '/login');
-	}
-
 	const user: User = await response.json();
-	
-	return { user };
-}) satisfies LayoutLoad;
+	if (user.emailVerifiedAt) {
+		redirect(303, '/dashboard');
+	}
+	return {
+		user
+	}
+}) satisfies PageLoad;
